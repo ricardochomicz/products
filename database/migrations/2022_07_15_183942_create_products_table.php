a2ee19951917cc55ace84af5dbd745b3fe159c0a
Ricardo <ricardo.chomicz@gmail.com>
2022-07-15 15:49:54 -0300

    models and database

---
+++ database/migrations/2022_07_15_183942_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->string('photo')->nullable();
+            $table->string('operation');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
